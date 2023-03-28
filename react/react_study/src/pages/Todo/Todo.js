3ee38f1797822b95e14c61e619e131431401a67c
0476a <0476a@naver.com>
2023-03-28 11:33:06 +0900

    2023.03.28(todo 수정)

--- react/react_study/src/pages/Todo/Todo.js
+++ react/react_study/src/pages/Todo/Todo.js
@@ -3,7 +3,7 @@ import { css } from '@emotion/react';
 import { FcPlus } from 'react-icons/fc';
 import { BiPen } from 'react-icons/bi';
 import { TiTrash } from 'react-icons/ti';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const TodoContainer = css`
     display: flex;
@@ -98,6 +98,13 @@ const ItemButton = css`
 
 const Todo = () => {
 
+    const [isOpen, setIsOpen] = useState(false);
+
+    const [modifyTodo, setModifyTodo] = useState({
+        id: 0,
+        content: ''
+    });
+
     const [input, setInput] = useState({
         id: 0,
         content: ''
@@ -115,11 +122,11 @@ const Todo = () => {
 
     const onKeyUp = (e) => {
         if(e.keyCode === 13) {
-            onClick();
+            onAdd();
         }
     }
 
-    const onClick = () => {
+    const onAdd = () => {
         const todo = {
             ...input,
             id: todoId.current++
@@ -136,27 +143,186 @@ const Todo = () => {
         ))
     }
 
+    const updateTodo = (modifyTodo) => {
+        setTodoList(
+            todoList.map(
+                todo => {
+                    if(todo.id === modifyTodo.id) {
+                        todo.content = modifyTodo.content;
+                    }
+                    return todo;
+                }
+            )
+        )
+    }
+
+    const openModal = (id) => {
+        setModifyTodo(todoList.filter(
+            todo => todo.id === id
+        )[0])
+        setIsOpen(true);
+    }
+
+    const modalContainer = css`
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: 99;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+        background-color: #000000aa;
+    `;
+
+    const modalBox = css`
+        border-radius: 7px;
+        width: 350px;
+        height: 200px;
+        background-color: #fafafa;
+        overflow: hidden;
+    `;
+
+    const modalHeader = css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-bottom: 1px solid #dbdbdb;
+        height: 40px;
+    `;
+
+    const modalTitle = css`
+        font-size: 18px;
+        font-weight: 600;
+    `;
+
+    const modalMain = css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-bottom: 1px solid #dbdbdb;
+        height: 120px;
+    `;
+
+    const modalInput = css`
+        outline: none;
+        border: none;
+        border-bottom: 2px solid green;
+        width: 90%;
+        height: 30px;
+        background-color: #fafafa;
+    `;
+
+    const modalFooter = css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 40px;
+    `;
+
+    const modalButton = css`
+        border: none;
+        background-color: #ffffff00;
+        width: 50%;
+        height: 100%;
+        font-weight: 600;
+        cursor: pointer;
+        &:first-of-type{
+            border-right: 1px solid #dbdbdb;
+        }
+        &:hover{
+            background-color: #eee; 
+        }
+        &:active{
+            background-color: #dbdbdb;
+        }
+    `;
+
+    const Modal = (props) => {
+
+        const modalRef = useRef();
+        const [modalContent, setModalContent] = useState('');
+
+        // 컴포넌트가 나타날 때(마운트) 사라질 때(언마운트) 사용
+        useEffect(
+            () => {
+                setModalContent(props.todo.content);
+                
+                const handler = (e) => {
+                    if(!modalRef.current.contains(e.target)) {
+                        props.setIsOpen(false);
+                    }
+                }
+                
+                document.addEventListener('mousedown', handler);
+                return () => {
+                    setModalContent(props.todo.content);
+                    document.removeEventListener('mousedown', handler);
+                }
+            },[]
+        );
+
+
+        const closeModal = () => {
+            props.setIsOpen(false);
+        }
+
+        const contentChange = (e) => {
+            setModalContent(e.target.value);
+        }
+
+        const onSubmit = () => {
+            props.updateTodo({
+                id: props.todo.id,
+                content: modalContent
+            });
+            closeModal();
+        }
+
+        return(
+            <div css={modalContainer}>
+                <div css={modalBox}  ref={modalRef}>
+                    <header css={modalHeader}>
+                        <h1 css={modalTitle}>{props.title}</h1>
+                    </header>
+                    <main css={modalMain}>
+                        <input css={modalInput} type="text" onChange={contentChange} defaultValue={props.todo.content}/>
+                    </main>
+                    <footer css={modalFooter}>
+                        <button css={modalButton} onClick={onSubmit}>확인</button>
+                        <button css={modalButton} onClick={closeModal}>취소</button>
+                    </footer>
+                </div>
+            </div>
+        );
+    }
+
 
     return (
-        <div css={TodoContainer}>
-            <div css={TodoAddition}>
-                <input css={AdditionInput} type="text" placeholder="Add your new Todo" onChange={onChange} onKeyUp={onKeyUp} value={input.content}/>
-                <button css={TodoAddButton} onClick={onClick}><FcPlus /></button>
+        <>
+            <div css={TodoContainer}>
+                <div css={TodoAddition}>
+                    <input css={AdditionInput} type="text" placeholder="Add your new Todo" onChange={onChange} onKeyUp={onKeyUp} value={input.content}/>
+                    <button css={TodoAddButton} onClick={onAdd}><FcPlus /></button>
+                </div>
+                {todoList.map(
+                    todo => {
+                        return (
+                            <div css={TodoList} key={todo.id}>
+                                <div css={TodoContent}>{todo.content}</div>
+                                <div css={ItemGroup}>
+                                    <button css={ItemButton} onClick={() => openModal(todo.id)}><BiPen /></button>
+                                    <button css={ItemButton} onClick={() => onRemove(todo.id)}><TiTrash/></button>
+                                </div>
+                            </div>      
+                        );
+                    }
+                )}
             </div>
-            {todoList.map(
-                todo => {
-                    return (
-                        <div css={TodoList} key={todo.id}>
-                            <div css={TodoContent}>{todo.content}</div>
-                            <div css={ItemGroup}>
-                                <button css={ItemButton}><BiPen /></button>
-                                <button css={ItemButton} onClick={() => onRemove(todo.id)}><TiTrash/></button>
-                            </div>
-                        </div>      
-                    );
-                }
-            )}
-        </div>
+            {isOpen ? (<Modal title={'Edit Todo'} todo={modifyTodo} setIsOpen={setIsOpen} updateTodo={updateTodo}/>) : ''}
+        </>
+        
     );
 };
 
